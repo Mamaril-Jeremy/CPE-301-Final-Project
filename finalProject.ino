1b9fa26e7a565f2a30f3e79e03e033cc2cca994e
Mamaril-Jeremy <89481128+Mamaril-Jeremy@users.noreply.github.com>
2023-04-13 11:14:10 -0700

    Update finalProject.ino

--- finalProject.ino
+++ finalProject.ino
@@ -1,5 +1,15 @@
+#define RDA 0x80
+#define TBE 0x20  
+
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+
+
 void setup(){
-  
+  U0init(9600);
   
 }
 
@@ -8,3 +18,33 @@ void loop(){
   
   
 }
+
+//Start of UART functions
+void U0init(unsigned long U0baud)
+{
+  unsigned long FCPU = 16000000;
+  unsigned int tbaud;
+  tbaud = (FCPU / 16 / U0baud - 1);
+  // Same as (FCPU / (16 * U0baud)) - 1;
+  *myUCSR0A = 0x20;
+  *myUCSR0B = 0x18;
+  *myUCSR0C = 0x06;
+  *myUBRR0  = tbaud;
+}
+
+unsigned char U0kbhit()
+{
+  return (*myUCSR0A & RDA);
+}
+
+unsigned char U0getchar()
+{
+  return *myUDR0;
+}
+
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A &= TBE) == 0){};
+  *myUDR0 = U0pdata;
+}
+//End of UART functions
