aef8ce94a5ce240f167d03cef6869d3e36b054a4
Mamaril-Jeremy <89481128+Mamaril-Jeremy@users.noreply.github.com>
2023-04-17 20:24:07 -0700

    Update finalProject.ino

--- finalProject.ino
+++ finalProject.ino
@@ -9,14 +9,31 @@
 volatile unsigned char *portDDRB = (unsigned char *) 0x24; //Yellow LED
 volatile unsigned char *portB = (unsigned char *) 0x25;
 
+volatile unsigned char *portDDRB = (unsigned char *) 0x24; //Green LED
+volatile unsigned char *portB = (unsigned char *) 0x25;
+
+volatile unsigned char *portDDRB = (unsigned char *) 0x24; //Red LED
+volatile unsigned char *portB = (unsigned char *) 0x25;
+
+volatile unsigned char *portDDRB = (unsigned char *) 0x24; //Blue LED
+volatile unsigned char *portB = (unsigned char *) 0x25;
+
+//UART registers
 volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
 volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
+//ADC registers
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
+
 enum States {
-  DISABLED.
+  DISABLED,
   IDLE, 
   ERROR,
   RUNNING,
@@ -25,6 +42,7 @@ enum States {
 
 void setup(){
   U0init(9600);
+  
   
 }
 
